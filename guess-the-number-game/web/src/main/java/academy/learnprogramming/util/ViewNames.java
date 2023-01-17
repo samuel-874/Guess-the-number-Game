ed7659231af9e1e13a3ce20b42b47dab0c5ea28f
samuel-874 <122874286+samuel-874@users.noreply.github.com>
2023-01-17 15:04:44 +0100

    this was a demo project

---
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+    //== CONSTANTS==
+    public static final String  HOME ="home";
+    public static final String  PLAY ="play";
+    public static final String  GAME_OVER ="game-over";
+
+    //==constructors==
+    private ViewNames(){}
+}
